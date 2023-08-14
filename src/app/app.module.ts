c523db1ecdda563409f31e7d4207727741a5df44
subhrachk <subhra_chk@hotmail.co.uk>
2023-08-15 00:50:56 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth-guard.service';
-
-const appRoutes : Routes = [ 
-  {path:'',component:HomeComponent}, 
-  {path:'users',component:UsersComponent,
-        children : [ 
-           {path:':id/:name',component:UserComponent}
-        ]
-  },
-  { path : 'servers', canActivateChild: [AuthGuard], component: ServersComponent, 
-        children:[ 
-        { path : ':id', component: ServerComponent}, 
-        { path : ':id/edit', component: EditServerComponent} 
-  ]}, 
- 
-] ;
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
+import { LoggingService } from './logging.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    UsersComponent,
-    ServersComponent,
-    UserComponent,
-    EditServerComponent,
-    ServerComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes)
+    HttpClientModule,
+    AppRoutingModule,
+    SharedModule,
+    CoreModule
   ],
-  providers: [ServersService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  // providers: [LoggingService]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y'
 };
 
 /*
